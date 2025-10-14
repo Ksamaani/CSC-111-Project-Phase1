8a83e83c7908c11f225925ea73c2d70c10860e8e
ksamaani <Ksamaani@gmail.com>
2025-10-14 14:30:12 +0300

    InitialCommit

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,2 +1,10 @@
+//This Project Is Done By:
+// Khalid ALSamaani     446100885
+
+import java.util.Scanner;
 public class LibrarySimulator {
+    public static void main (String[] args){
+        Scanner Input = new Scanner(System.in);
+
+    }
 }
