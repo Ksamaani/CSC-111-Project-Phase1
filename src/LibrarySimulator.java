01b6b5ed21c1bb631a6030bcd01f8fc8b9dc6759
ksamaani <ksamaani@gmail.com>
2025-10-14 16:24:00 +0300

    initial Commit

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,7 +1,6 @@
 //This Project Is Done By:
 // Khalid ALSamaani     446100885
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class LibrarySimulator {
     public static void main (String[] args){
