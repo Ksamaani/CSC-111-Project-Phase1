f65ffae9a92e48ebfc8ed31d004af49f67f73416
ksamaani <Ksamaani@gmail.com>
2025-10-15 13:21:46 +0300

    Variables Add

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class LibrarySimulator {
     public static void main (String[] args){
         Scanner Input = new Scanner(System.in);
+        //user Variables
+        String uesr1name = "Ahmad";
+        int user1Id = 101;
+        int user1BorrowedBook = 0;
+        String uesr2name = "Mohamad";
+        int user2Id = 202;
+        int user2BorrowedBook = 0;
+        String uesr3name = "Nasser";
+        int user3Id = 303;
+        int user3BorrowedBook = 0;
+        //Session Variables
+        int SessionBorrowedBooks = 0;
+        int SessionReturnedBooks = 0;
+        //golbal Variables
+        double totalRevenue = 0.0;
+        int totalBorrowedOperations = 0;
+        int totalReturnedOperations = 0;
+
 
     }
 }
